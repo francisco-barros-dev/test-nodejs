3e97c52bedcfe24b86a5d975472108727e0f1ea1
francisco-barros-dev <cr3d1n84@gmail.com>
2024-01-10 20:28:51 -0300

    feat: add jest config file

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+  ],
+}
